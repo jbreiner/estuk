393d602db106ed7a420e4e00b89c99825fb04bfa
John Breiner <jbreiner@gmail.com>
2015-02-13 08:09:04 -0600

    Added the books scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125200559) do
+ActiveRecord::Schema.define(version: 20150213015837) do
+
+  create_table "books", force: true do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "description"
+    t.integer  "price"
+    t.boolean  "availability", default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "installs", force: true do |t|
     t.string   "email",                  default: "", null: false
